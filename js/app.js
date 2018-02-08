31541dff49e8d6ab19a94f640d3675be9cb44554
SIlviaTivi <skfl.erg@hotmail.com>
2018-02-07 19:38:12 -0500

    ruta en bicicleta

--- js/app.js
+++ js/app.js
@@ -9,7 +9,7 @@ function initMap() {
   new google.maps.places.Autocomplete(inputDestiny);
   /* Fin de  Autocompletado */
   var map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 18,
+    zoom: 15,
     center: laboratoriaLima
   });
   var marker = new google.maps.Marker({
@@ -21,11 +21,13 @@ function initMap() {
   /* trazar ruta */
   var directionsService = new google.maps.DirectionsService;
   var directionsDisplay = new google.maps.DirectionsRenderer;
+
   var calculateAndDisplayRoute = function(directionsService, directionsDisplay) {
     directionsService.route({
       origin: inputGoing.value,
       destination: inputDestiny.value,
       travelMode: 'DRIVING'
+
     }, function(response, status) {
       if (status === 'OK') {
         directionsDisplay.setDirections(response);
@@ -34,11 +36,35 @@ function initMap() {
       }
     });
   };
+
+  //Ruta en bicicleta
+  var calculateAndDisplayRouteBicycling = function(directionsService, directionsDisplay) {
+    directionsService.route({
+      origin: inputGoing.value,
+      destination: inputDestiny.value,
+      travelMode: 'BICYCLING'
+
+    }, function(response, status) {
+      if (status === 'OK') {
+        directionsDisplay.setDirections(response);
+      } else {
+        window.alert('No hay ruta para bicicletas');
+      }
+    });
+  }; //Ruta en bicicleta fin
+
   directionsDisplay.setMap(map);
+
   var trazarRuta = function() {
     calculateAndDisplayRoute(directionsService, directionsDisplay);
   };
   document.getElementById('trazar-ruta').addEventListener('click', trazarRuta);
+
+  //funcion ruta en bicicleta 
+  var trazarRutaBicycling = function() {
+    calculateAndDisplayRouteBicycling(directionsService, directionsDisplay);
+  };
+  document.getElementById('ruta-bicicleta').addEventListener('click', trazarRutaBicycling); //funcion ruta en bicicleta fin
   /* Fin de trazar ruta */
 }
 function buscar() {
@@ -50,7 +76,7 @@ function buscar() {
       longitud = posicion.coords.longitude;
      
       var map = new google.maps.Map(document.getElementById('map')); 
-      map.setZoom(18);
+      map.setZoom(15);
       map.setCenter({lat: latitud,
         lng: longitud});
 
